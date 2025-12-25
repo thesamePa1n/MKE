48b14dc9fb8b9202e790f0309f5e599b9de93054
VasyaPain2004 <VasyaPain2004@gmail.com>
2025-12-25 23:59:25 +0900

    laba9

--- vlad-9/2.py
+++ vlad-9/2.py
@@ -28,14 +28,44 @@ for ci in range(Nc):
   FF[dof0] += f * h / 2
   FF[dof1] += f * h / 2
   
-print(AA)
-print() 
+AA[0, :] = 0
+AA[-1, :] = 0
+AA[0, 0] = 1.0
+AA[-1, -1] = 1.0
+FF[0] = 5.0
+FF[-1] = 0.0
+  
+U = numpy.linalg.solve(AA, FF)
+print("Our solve")
+print(U)
+print()
 
 V = FunctionSpace(mesh, "CG", 1)
 u = TrialFunction(V)
 v = TestFunction(V)
 kk = Constant(k)
+ff = Constant(f)
 a = inner(kk * grad(u), grad(v)) * dx
+L = ff * v * dx
+
+def left_boundary(x, on_boundary):
+    return on_boundary and x[0] < DOLFIN_EPS
+
+def right_boundary(x, on_boundary):
+    return on_boundary and x[0] > 1.0 - DOLFIN_EPS
+
+bc_left = DirichletBC(V, Constant(5.0), left_boundary)
+bc_right = DirichletBC(V, Constant(0.0), right_boundary)
+
 A = Matrix()
+b = Vector()
 assemble(a, A)
-print(A.array()) 
+assemble(L, b)
+bc_left.apply(A, b)
+bc_right.apply(A, b)
+
+u = Function(V)
+solve(A, u.vector(), b)
+
+print('Fenics solve')
+print(u.vector().get_local())
